1df0ee7fe184229d9c4a5585c0cc573b92fc1cd4
ignacioPL <perezlaborda@gmail.com>
2019-03-09 11:35:31 -0300

    more slow service

--- app/services/SlowService.scala
+++ app/services/SlowService.scala
@@ -4,9 +4,11 @@ class SlowService {
 
   private val a: BigInt = BigInt(Long.MaxValue) * BigInt(Long.MaxValue)
   private val b: BigInt = BigInt(Long.MaxValue) * BigInt(Long.MaxValue)
-
+  //71000000
   def loopIterator(count: Int): Unit = {
-    (0 to count).foreach{ _ => val c = a*b }
+    (0 to count).foreach{ i =>
+      val c = a*b*BigInt(i)
+    }
   }
 
 }
